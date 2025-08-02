e0aa5777a4e2f3a0eeb0d6e72a53b253021d370c
anjali91106 <anjalisoni86904@gmail.com>
2025-08-02 13:51:41 +0530

    fixing the fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,3 +2,4 @@ import {Inter} from 'next/font/google'
 
 export const inter = Inter({subsets: ['latin']});
 export const font2 = Inter({subsets: ['vietnamese'], weight: "700"})
+export const lusitana = Inter({subsets: ['vietnamese'], weight: "700"})
